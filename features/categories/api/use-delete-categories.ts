06a5413349ba8d98a3b35ec9ee2aead58e59b470
MohdBilal <mohdbilal2870@gmail.com>
2024-10-05 09:56:18 +0530

    added summary queries

--- features/categories/api/use-delete-categories.ts
+++ features/categories/api/use-delete-categories.ts
@@ -24,7 +24,8 @@ export const useDeleteCategory = (id?: string) => {
       queryClient.invalidateQueries({ queryKey: ["category", { id }] });
       queryClient.invalidateQueries({ queryKey: ["categories"] });
       queryClient.invalidateQueries({ queryKey: ["transactions"] });
-      // queryClient.invalidateQueries({ queryKey: ["summary"] });
+      
+      queryClient.invalidateQueries({ queryKey: ["summary"] });// queryClient.invalidateQueries({ queryKey: ["summary"] });
     },
     onError: () => {
       toast.error("Failed to delete category.");
